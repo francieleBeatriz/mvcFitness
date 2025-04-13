c4abe0d94f04b53ac74f26aa020e74588e8f9cb2
Pablio Richardy <yr0p@protonmail.com>
2025-04-13 13:40:51 -0300

    Removendo index.php e Autoload.php do versionamento e adicionando ao .gitignore

--- generic/AutoLoad.php
+++
@@ -1,8 +0,0 @@
-<?php
-spl_autoload_register(function ($class){
-    $caminho = __DIR__ . '/../' . str_replace('\\', '/', $class) . '.php';
-
-    if (file_exists($caminho)) {
-        include $caminho;
-    }
-});
